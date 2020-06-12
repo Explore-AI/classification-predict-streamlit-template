c2fa9dfbfdb87acb7691edccf4f5d391217c40b8
PythonMaester <60380304+PythonMaester@users.noreply.github.com>
2020-06-12 15:11:33 +0200

    made a new model for classifying tweets, added test and train csv

--- base_app.py
+++ base_app.py
@@ -21,19 +21,48 @@
 	https://docs.streamlit.io/en/latest/
 
 """
+#---------------------------------------------------------------
 # Streamlit dependencies
 import streamlit as st
-import joblib,os
 
 # Data dependencies
+import spacy
+nlp = spacy.load('en_core_web_sm')
 import pandas as pd
+import numpy as np
 
-# Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
-tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
+# Enter your code here:
+dftrain = pd.read_csv('train.csv')
+dftest = pd.read_csv('test.csv')
 
-# Load your raw data
-raw = pd.read_csv("resources/train.csv")
+# splitting the model
+from sklearn.model_selection import train_test_split
+
+X = dftrain['message']  # this time we want to look at the text
+y = dftrain['sentiment']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# since i am going to use the tfidtransformer and linearsvc, i should thus create their objects first
+
+# create tfidf object
+from sklearn.feature_extraction.text import TfidfVectorizer
+# tfidf_transformer = TfidfTransformer()
+
+# create linearsvc object
+from sklearn.svm import LinearSVC
+# clf = LinearSVC()
+
+from sklearn.pipeline import Pipeline
+
+text_clf = Pipeline([('tfidf', TfidfVectorizer()),
+                     ('clf', LinearSVC()),
+])
+
+# Feed the training data through the pipeline
+text_clf.fit(X_train, y_train)  
+
+
 
 # The main function where we will build the actual app
 def main():
@@ -41,13 +70,17 @@ def main():
 
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.title("Tweet Classifer")
+	st.title("Tweet Classifer/ better model")
 	st.subheader("Climate change tweet classification")
+	st.subheader("Does this streamlit thing really work though?")
+	st.text('yes it works now i need a proper model, a really working model')
 
+	
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
 	options = ["Prediction", "Information"]
 	selection = st.sidebar.selectbox("Choose Option", options)
+
 
 	# Building out the "Information" page
 	if selection == "Information":
@@ -67,15 +100,16 @@ def main():
 
 		if st.button("Classify"):
 			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			#vect_text = tweet_cv.transform([tweet_text]).toarray()
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
+			#predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			#prediction = predictor.predict(vect_text)
 
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
+			prediction =text_clf.predict([tweet_text])
 			st.success("Text Categorized as: {}".format(prediction))
 
 # Required to let Streamlit instantiate our web app.  
