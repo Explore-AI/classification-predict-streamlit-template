7d6e85483082f7ce4e60f05313b54d90f441438e
Xenaschke <xcroucamp@gmail.com>
2020-06-23 21:09:43 +0200

    Update

--- base_app.py
+++ base_app.py
@@ -43,7 +43,7 @@ def main():
 	# these are static across all pages
 	st.title("Tweet Classifer")
 	st.subheader("Climate change tweet classification")
-	st.markdown("You can now choose which model you would like to try out (we reccomend the neural network though).")
+    
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
 	options = ["Prediction", "Data Insights", "App Overview", "Climate Change News"]
@@ -62,6 +62,7 @@ def main():
 	# Building out the predication page
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
+		st.markdown("You can now choose which model you would like to try out (we reccomend the neural network though).")        
         # Creating selection box to be able to choose a model
 		model_option = st.selectbox('You can choose one of the following models:',['Logistic Regression','Naïve Bayes','Linear SVM (Support Vector Machine)','Random Forest','KNN (K Nearest Neighbors)','Neural Networks'])
 		# Creating a text box for user input
@@ -80,23 +81,23 @@ def main():
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
 
-# # Building out the Climate Change News page
-# 	if selection == "Climate Change News":
-# 		st.info("What is climate change? How does it impact us? Am I to blame?")  
-# 		# You can read a markdown file from supporting resources folder
-# 		st.markdown("Some information here")
+# Building out the Climate Change News page
+	if selection == "Climate Change News":
+		st.info("What is climate change? How does it impact us? Am I to blame?")  
+		# You can read a markdown file from supporting resources folder
+		st.markdown("Some information here")
         
-# # Building out the Data Insights page
-# 	if selection == "Data Insights":
-# 		st.info("A few insights from the dataset.")  
-# 		# You can read a markdown file from supporting resources folder
-# 		st.markdown("Some information here")
+# Building out the Data Insights page
+	if selection == "Data Insights":
+		st.info("A few insights from the dataset.")  
+		# You can read a markdown file from supporting resources folder
+		st.markdown("Some information here")
         
-#         st.subheader("View visuals")
-# 		if st.checkbox('Distribution of different classes for tweets'): # data is hidden if box is unchecked
-# 			st.write() # will write the df to the page
-#  		if st.checkbox('Most common words'): # data is hidden if box is unchecked
-# 			st.write() # will write the df to the page
+		st.subheader("View visuals")
+		if st.checkbox('Distribution of different classes for tweets'): # data is hidden if box is unchecked
+			st.write() # will write the df to the page
+ 		if st.checkbox('Most common words'): # data is hidden if box is unchecked
+			st.write() # will write the df to the page
         
         
 # Required to let Streamlit instantiate our web app.  
