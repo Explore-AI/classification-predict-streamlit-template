2ef53d28ae0423f21313e21927f02bdd667e0957
georgetshego <60339076+georgetshego@users.noreply.github.com>
2020-06-25 16:04:07 +0200

    edit

--- base_app2.py
+++ base_app2.py
@@ -20,57 +20,84 @@ def main():
     
     #title and subheader 
     st.title("Tweet Classifer App")
-    st.subheader("Climate change tweet classification")
     #creating side menu
-    options = ["Information","Lr","Lsvm","Rf","Knn","Nn","Visuals"]
+    options = ["About the app","Models","Visuals"]
     selection = st.sidebar.selectbox("Menu Options", options)
 
     #building the Information page
-    if selection == "Information":
-        st.info("General Information")
+    if selection == "About the app":
+        st.subheader("About the climate change tweet classififer app")
+        st.markdown("![Image of Yaktocat](https://abcsplash-bc-a.akamaized.net/4477599164001/201604/4477599164001_4864948520001_4863149671001-vs.jpg?pubId=4477599164001.jpg)")
+        st.markdown("This climate change tweet classifier app is useful for classifying whether or not a person believes in climate change, based on their tweet.")
+        st.markdown("The App is created to help companies determine how people perceive climate change and whether or not they believe it is a real threat. This would add to their market research efforts in gauging how their product/service may be received. The App gives users a choice to use a model of their choice to determine how the tweet(s) they have percieve(s) climate change.")
         # You can read a markdown file from supporting resources folder
-        st.markdown("This climate change tweet classifier app is useful for classifying whether or not a person believes in climate change, based on their tweet. \n The App is created to help companies determine how people perceive climate change and whether or not they believe it is a real threat. This would add to their market research efforts in gauging how their product/service may be received. The App gives users a choice to use a model of their choice to determine how the tweet(s) they have percieve(s) climate change')
         st.subheader("Raw Twitter data and label")
         if st.checkbox('Show raw data'): # data is hidden if box is unchecked
             st.write(raw[['sentiment', 'message']]) # will write the df to the page
-
-    #building the Logistic Regression
-    if selection == "Lr":
-        st.info("Prediction with Logistic Regression Model")
-        tweet_text = st.text_area("enter text ","Type Here")
-        if st.button("Classify"):
-            # Transforming user input into a list
-            vect_text = [tweet_text]
-            # Load .pkl file with the model of your choice + make predictions
-            predictor = joblib.load(open(os.path.join("lr.pkl"),"rb"))
-            prediction = predictor.predict(vect_text)
-            st.success("Text Categorized as: {}".format(prediction))
-
-    #building the Naive Bayes
-    if selection == "Nn":
-        st.info("Prediction with Naive Bayes Model")
-        tweet_text = st.text_area("enter text ","Type Here")
-        if st.button("Classify"):
-            # Transforming user input into a list
-            vect_text = [tweet_text]
-            # Load .pkl file with the model of your choice + make predictions
-            predictor = joblib.load(open(os.path.join("lr.pkl"),"rb"))
-            prediction = predictor.predict(vect_text)
-            st.success("Text Categorized as: {}".format(prediction))
+    
+    if selection== "Models":
         
-    #building the Linear SVM
-    if selection == "Lsvm":
-        st.info("Prediction with Linear SVM Model")
-        tweet_text = st.text_area("enter text ","Type Here")
-        if st.button("Classify"):
-            # Transforming user input into a list
-            vect_text = [tweet_text]
-            # Load .pkl file with the model of your choice + make predictions
-            predictor = joblib.load(open(os.path.join("Linear_SVM_base_model.pkl"),"rb"))
-            prediction = predictor.predict(vect_text)
-            st.success("Text Categorized as: {}".format(prediction))
-            
-        #building the Draw
+        models = pd.DataFrame({'model name': ['Logistic Regression', 'Naive Bayes','Linear SVM','Random Forest', 'K Nearest Neighbors']})
+        model_sel=st.selectbox('Select a model', models['model name'])
+    
+        #building the Logistic Regression
+        if model_sel == "Logistic Regression":
+            st.info("Prediction with Logistic Regression Model")
+            tweet_text = st.text_area("enter text ","Type Here")
+            if st.button("Classify"):
+                # Transforming user input into a list
+                vect_text = [tweet_text]
+                # Load .pkl file with the model of your choice + make predictions
+                predictor = joblib.load(open(os.path.join("lr.pkl"),"rb"))
+                prediction = predictor.predict(vect_text)
+                st.success("Text Categorized as: {}".format(prediction))
+
+        #building the Naive Bayes
+        if model_sel == "Naive Bayes":
+            st.info("Prediction with Naive Bayes Model")
+            tweet_text = st.text_area("enter text ","Type Here")
+            if st.button("Classify"):
+                # Transforming user input into a list
+                vect_text = [tweet_text]
+                # Load .pkl file with the model of your choice + make predictions
+                predictor = joblib.load(open(os.path.join("lr.pkl"),"rb"))
+                prediction = predictor.predict(vect_text)
+                st.success("Text Categorized as: {}".format(prediction))
+        
+        #building the Linear SVM
+        if model_sel == "Linear SVM":
+            st.info("Prediction with Linear SVM Model")
+            tweet_text = st.text_area("enter text ","Type Here")
+            if st.button("Classify"):
+                # Transforming user input into a list
+                vect_text = [tweet_text]
+                # Load .pkl file with the model of your choice + make predictions
+                predictor = joblib.load(open(os.path.join("Linear_SVM_base_model.pkl"),"rb"))
+                prediction = predictor.predict(vect_text)
+                st.success("Text Categorized as: {}".format(prediction))
+        #building the Random Forest
+        if model_sel == "Random Forest":
+            st.info("Prediction with Random Forest Model")
+            tweet_text = st.text_area("enter text ","Type Here")
+            if st.button("Classify"):
+                # Transforming user input into a list
+                vect_text = [tweet_text]
+                # Load .pkl file with the model of your choice + make predictions
+                predictor = joblib.load(open(os.path.join("lr.pkl"),"rb"))
+                prediction = predictor.predict(vect_text)
+                st.success("Text Categorized as: {}".format(prediction))   
+        #building the KNN
+        if model_sel == "K Nearest Neighbors":
+            st.info("Prediction with K Nearest Neighbors Model")
+            tweet_text = st.text_area("enter text ","Type Here")
+            if st.button("Classify"):
+                # Transforming user input into a list
+                vect_text = [tweet_text]
+                # Loading .pkl file with the model of your choice + make predictions
+                predictor = joblib.load(open(os.path.join("lr.pkl"),"rb"))
+                prediction = predictor.predict(vect_text)
+                st.success("Text Categorized as: {}".format(prediction))
+    #building the Draw
     if selection == "Visuals":
         plt.figure(figsize=(8.5,5))
         raw['sentiment'].replace({-1: 'Anti',0:'Neutral',1:'Pro',2:'News'}).value_counts().plot(kind='bar',figsize=(8.5,5), color='tan')
@@ -82,29 +109,6 @@ def main():
         sns.distplot(raw['sentiment'],color='g',kde_kws={'bw':0.1}, bins=100, hist_kws={'alpha': 0.4})
         plt.title('Distribution graph for different classes')
         st.pyplot()
-    #building the Random Forest
-    if selection == "Rf":
-        st.info("Prediction with Random Forest Model")
-        tweet_text = st.text_area("enter text ","Type Here")
-        if st.button("Classify"):
-            # Transforming user input into a list
-            vect_text = [tweet_text]
-            # Load .pkl file with the model of your choice + make predictions
-            predictor = joblib.load(open(os.path.join("lr.pkl"),"rb"))
-            prediction = predictor.predict(vect_text)
-            st.success("Text Categorized as: {}".format(prediction))        
-
-    #building the KNN
-    if selection == "Knn":
-        st.info("Prediction with K Nearest Neighbors")
-        tweet_text = st.text_area("enter text ","Type Here")
-        if st.button("Classify"):
-            # Transforming user input into a list
-            vect_text = [tweet_text]
-            # Loading .pkl file with the model of your choice + make predictions
-            predictor = joblib.load(open(os.path.join("lr.pkl"),"rb"))
-            prediction = predictor.predict(vect_text)
-            st.success("Text Categorized as: {}".format(prediction))
 
 
 # Required to let Streamlit instantiate our web app.  
