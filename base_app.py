7de87d4658d00f20bb3bee776af87815308ff191
wisley94 <wramukhuba@gmail.com>
2021-06-16 12:16:36 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -27,22 +27,139 @@ import joblib,os
 
 # Data dependencies
 import pandas as pd
+import numpy as np
+import re
+import inflect
+import contractions
+import string
+import unicodedata
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from nltk.stem import WordNetLemmatizer
+import urllib
+import nltk
+from nltk.corpus import stopwords
+from nltk import TreebankWordTokenizer
 
 # Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
+news_vectorizer = open("resources/vectoriser.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
 
+# preprocessing the raw data
+
+         # first function
+def remove_URL(words):
+    """This function take a string as an input and removes any url that are present in that string"""
+    
+    return re.sub(r"http\S+", "", words)
+
+raw['message'] = raw['message'].apply(remove_URL)
+
+        # second function
+def incomplete_words(words):
+    """
+    This function replace all incomplete word with the original version of the word
+    For example, `I'll` will become `I will` and `gng
+    """
+    return contractions.fix(words)
+
+raw['message'] = raw['message'].apply(incomplete_words)
+
+       # third function
+def remove_punctuations(tokenized_words):
+    """This function take a string/list as an input and removes all the punctuations"""
+    words = str.maketrans('', '', string.punctuation)
+    return tokenized_words.translate(words)
+
+def replace_slang(text):       
+    return ' '.join([word.replace('rt', '') for word in text.split()])
+
+def remove_non_ascii(tokenized_words):
+    """Remove non-ASCII characters from list of tokenized words"""
+
+    return ''.join([unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore') for word in tokenized_words])
+
+def to_lowercase(tokenized_words):
+    """Convert all characters to lowercase from list of tokenized words"""
+    
+    return ''.join([word.lower() for word in tokenized_words])
+
+def replace_numbers(tokenized_words):
+    """Replace all interger occurrences in list of tokenized words with textual representation"""
+    inflector = inflect.engine()
+
+    return ''.join([inflector.number_to_words(word) if word.isdigit() else word for word in tokenized_words])
+
+def remove_other(text):
+    
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    
+    return text
+
+def remove_emoji(words):
+    emoji_pattern = re.compile("["
+                u"\U0001F600-\U0001F64F"
+                u"\U0001F300-\U0001F5FF"
+                u"\U0001F680-\U0001F6FF"
+                u"\U0001F1E0-\U0001F1FF"
+                u"\U00002702-\U000027B0"
+                u"\U000024C2-\U0001F251"
+                u"\U0001f926-\U0001f937"
+                u'\U00010000-\U0010ffff'
+                u"\u200d"
+                u"\u2640-\u2642"
+                u"\u2600-\u2B55"
+                u"\u23cf"
+                u"\u23e9"
+                u"\u231a"
+                u"\u3030"
+                u"\ufe0f"
+    "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', words)
+
+def clean_text_data(tokenized_words):
+    """Clean each word from list of tokenized words"""
+    
+    tokenized_words = remove_non_ascii(tokenized_words)
+    tokenized_words = remove_emoji(tokenized_words)
+    tokenized_words = replace_numbers(tokenized_words)
+    tokenized_words = to_lowercase(tokenized_words)
+    tokenized_words = remove_punctuations(tokenized_words)
+    tokenized_words = remove_other(tokenized_words)
+    tokenized_words = replace_slang(tokenized_words)
+    
+    return tokenized_words
+
+raw['message'] = raw['message'].apply(clean_text_data)
+
+    #fourth function
+def remove_stopwords(text):
+    """Remove stop words from list of tokenized words"""
+    text = ' '.join([word for word in text.split() if word not in stopwords.words('english')])
+    return text
+
+raw['message'] = raw['message'].apply(remove_stopwords)
+
+
+
 # The main function where we will build the actual app
 def main():
 	"""Tweet Classifier App with Streamlit """
 
-	# Creates a main title and subheader on your page -
+	# Creates a main title and subheader on your page
 	# these are static across all pages
 	st.title("Tweet Classifer")
-	st.subheader("Climate change tweet classification")
+	st.subheader("Climate change tweet classification by AM1")
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
@@ -70,7 +187,7 @@ def main():
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			predictor = joblib.load(open(os.path.join("resources/trained.pkl"),"rb"))
 			prediction = predictor.predict(vect_text)
 
 			# When model has successfully run, will print prediction
