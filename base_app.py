871d61b6d9ab2aa2d7596d60b486e0d67abb7903
wisley94 <wramukhuba@gmail.com>
2021-06-20 23:04:32 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -42,50 +42,39 @@ raw = pd.read_csv("resources/train.csv")
 
 # preprocessing the raw data
 
-         # first function
-def remove_URL(words):
-    """This function take a string as an input and removes any url that are present in that string"""
+def pre_process(text):
+    # remove stpo_words
+    text = ' '.join([word for word in text.split() if word not in stopwords.words('english')])
     
-    return re.sub(r"http\S+", "", words)
-
-raw['message'] = raw['message'].apply(remove_URL)
-
-       # third function
-def remove_punctuations(tokenized_words):
-    """This function take a string/list as an input and removes all the punctuations"""
+    # This function take a string as an input and removes any url that are present in that string
+    text =  re.sub(r"http\S+", "", text)
+    
+    # remove punctuations
     words = str.maketrans('', '', string.punctuation)
-    return tokenized_words.translate(words)
-
-def replace_slang(text):       
-    return ' '.join([word.replace('rt', '') for word in text.split()])
-
-def remove_non_ascii(tokenized_words):
-    """Remove non-ASCII characters from list of tokenized words"""
-
-    return ''.join([unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore') for word in tokenized_words])
-
-def to_lowercase(tokenized_words):
-    """Convert all characters to lowercase from list of tokenized words"""
+    text = text.translate(words)
     
-    return ''.join([word.lower() for word in tokenized_words])
-
-def replace_numbers(tokenized_words):
-    """Replace all interger occurrences in list of tokenized words with textual representation"""
+    
+    #replace slang
+    text = ' '.join([word.replace('rt', '') for word in text.split()])
+    
+    #remove ascii
+    text = ''.join([unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore') for word in text])
+    
+    # Convert all characters to lowercase from list of tokenized words
+    text = ''.join([word.lower() for word in text])
+    
+    # Replace all intergers
     inflector = inflect.engine()
-
-    return ''.join([inflector.number_to_words(word) if word.isdigit() else word for word in tokenized_words])
-
-def remove_other(text):
+    text = ''.join([inflector.number_to_words(word) if word.isdigit() else word for word in text])
     
+    # other
     text = re.sub('\[.*?\]', '', text)
     text = re.sub('<.*?>+', '', text)
     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
     text = re.sub('\n', '', text)
-    text = re.sub('\w*\d\w*', '', text)
+    text = re.sub('\w*\d\w*', '', text) 
     
-    return text
-
-def remove_emoji(words):
+    #
     emoji_pattern = re.compile("["
                 u"\U0001F600-\U0001F64F"
                 u"\U0001F300-\U0001F5FF"
@@ -103,32 +92,10 @@ def remove_emoji(words):
                 u"\u231a"
                 u"\u3030"
                 u"\ufe0f"
-    "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', words)
-
-def clean_text_data(tokenized_words):
-    """Clean each word from list of tokenized words"""
+     "]+", flags=re.UNICODE)
+    text = emoji_pattern.sub(r'', text)
     
-    tokenized_words = remove_non_ascii(tokenized_words)
-    tokenized_words = remove_emoji(tokenized_words)
-    tokenized_words = replace_numbers(tokenized_words)
-    tokenized_words = to_lowercase(tokenized_words)
-    tokenized_words = remove_punctuations(tokenized_words)
-    tokenized_words = remove_other(tokenized_words)
-    tokenized_words = replace_slang(tokenized_words)
-    
-    return tokenized_words
-
-raw['message'] = raw['message'].apply(clean_text_data)
-
-    #fourth function
-def remove_stopwords(text):
-    """Remove stop words from list of tokenized words"""
-    text = ' '.join([word for word in text.split() if word not in stopwords.words('english')])
     return text
-
-raw['message'] = raw['message'].apply(remove_stopwords)
-
 
 
 # The main function where we will build the actual app
@@ -159,21 +126,21 @@ def main():
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text","Type Here")
+		tweet_text = st.text_area("Enter Text",str("Type Here"))
 
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/trained.pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
+	if st.button("Classify"):
+			   # Transforming user input with vectorizer
+			    vect_text = tweet_cv.transform([pre_process(tweet_text)]).toarray()
+			    # Load your .pkl file with the model of your choice + make predictions
+			    # Try loading in multiple models to give the user a choice
+			    predictor = joblib.load(open(os.path.join("resources/trained.pkl"),"rb"))
+			    prediction = predictor.predict(vect_text)
 
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
+			    # When model has successfully run, will print prediction
+			    # You can use a dictionary or similar structure to make this output
+			    # more human interpretable.
+			    st.success("Text Categorized as: {}".format(prediction))
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
-	main()
+      main()
