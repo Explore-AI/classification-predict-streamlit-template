8d287e467994d25445f8fc5833a7ec73574c161f
Onkarabile Tshele <201706535@swave.smu.ac.za>
2021-06-23 14:49:45 +0200

    added sidebar image

--- base_app.py
+++ base_app.py
@@ -1,7 +1,7 @@
 """
 
     Simple Streamlit webserver application for serving developed classification
-	models.
+    models.
 
     Author: Explore Data Science Academy.
 
@@ -13,17 +13,18 @@
     ---------------------------------------------------------------------
 
     Description: This file is used to launch a minimal streamlit web
-	application. You are expected to extend the functionality of this script
-	as part of your predict project.
+    application. You are expected to extend the functionality of this script
+    as part of your predict project.
 
-	For further help with the Streamlit framework, see:
+    For further help with the Streamlit framework, see:
 
-	https://docs.streamlit.io/en/latest/
+    https://docs.streamlit.io/en/latest/
 
 """
 # Streamlit dependencies
 import streamlit as st
-import joblib,os
+import joblib
+import os
 
 # Data dependencies
 import pandas as pd
@@ -34,46 +35,50 @@ import unicodedata
 from nltk.corpus import stopwords
 
 # Vectorizer
-news_vectorizer = open("resources/vectoriser.pkl","rb")
-tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
+news_vectorizer = open("resources/vectoriser.pkl", "rb")
+# loading your vectorizer from the pkl file
+tweet_cv = joblib.load(news_vectorizer)
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
 
 # preprocessing the raw data
 
+
 def pre_process(text):
     # remove stpo_words
-    text = ' '.join([word for word in text.split() if word not in stopwords.words('english')])
-    
+    text = ' '.join([word for word in text.split()
+                    if word not in stopwords.words('english')])
+
     # This function take a string as an input and removes any url that are present in that string
-    text =  re.sub(r"http\S+", "", text)
-    
+    text = re.sub(r"http\S+", "", text)
+
     # remove punctuations
     words = str.maketrans('', '', string.punctuation)
     text = text.translate(words)
-    
-    
-    #replace slang
+
+    # replace slang
     text = ' '.join([word.replace('rt', '') for word in text.split()])
-    
-    #remove ascii
-    text = ''.join([unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore') for word in text])
-    
+
+    # remove ascii
+    text = ''.join([unicodedata.normalize('NFKD', word).encode(
+        'ascii', 'ignore').decode('utf-8', 'ignore') for word in text])
+
     # Convert all characters to lowercase from list of tokenized words
     text = ''.join([word.lower() for word in text])
-    
+
     # Replace all intergers
     inflector = inflect.engine()
-    text = ''.join([inflector.number_to_words(word) if word.isdigit() else word for word in text])
-    
+    text = ''.join([inflector.number_to_words(
+        word) if word.isdigit() else word for word in text])
+
     # other
     text = re.sub('\[.*?\]', '', text)
     text = re.sub('<.*?>+', '', text)
     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
     text = re.sub('\n', '', text)
-    text = re.sub('\w*\d\w*', '', text) 
-    
+    text = re.sub('\w*\d\w*', '', text)
+
     #
     emoji_pattern = re.compile("["
                 u"\U0001F600-\U0001F64F"
@@ -94,62 +99,63 @@ def pre_process(text):
                 u"\ufe0f"
      "]+", flags=re.UNICODE)
     text = emoji_pattern.sub(r'', text)
-    
+
     return text
 
 
 # The main function where we will build the actual app
 def main():
-	"""Tweet Classifier App with Streamlit """
+    """Tweet Classifier App with Streamlit """
 
-	# Creates a main title and subheader on your page
-	# these are static across all pages
-	st.title("Clamassifer")
-	st.subheader("You tweet, we classify!")
+    # Creates a main title and subheader on your page
+    # these are static across all pages
 
-	# Creating sidebar with selection box -
-	# you can create multiple pages this way
-	options = ["Introduction", "Prediction", "Information", "Team"]
-	selection = st.sidebar.selectbox("Choose Option", options)
+    st.title("Sentiment Analysis")
+    st.subheader("You tweet, we classify!")
+    st.sidebar.image("c2fea606b12a4a2ebdc4dd18e5cc9b54.png", use_column_width=True)
+
+    # Creating sidebar with selection box -
+    # you can create multiple pages this way
+    options = ["Introduction", "Prediction", "Information", "Team"]
+    selection = st.sidebar.selectbox("Choose Option", options)
     
     # Building out the "Information" page
-	if selection == "Introduction":
-		st.info("Climate Change")
+    if selection == "Introduction":
 
-		# You can read a markdown file from supporting resources folder
-		st.markdown("![climate](https://images.unsplash.com/photo-1580868636775-b8a1818ca086?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&h=300&q=80)  \n\n"
+        # You can read a markdown file from supporting resources folder
+        st.markdown("![climate](https://images.unsplash.com/photo-1580868636775-b8a1818ca086?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&h=300&q=80)  \n\n"
                     "Hey there! Welcome to Clamassifer App. An app that classifies climate change tweets.  \n\n"
                     "Are you a company planning to launch a product or offer a service that is environmentally friendly and sustainable? But you want to know how your customers perceive climate change?   \n\n"
                     "We got you! Clamassifer helps you understand how your products/service may be received so that you can come up with better marketing strategies and potentially increase your revenues.")
 
-	# Building out the "Information" page
-	if selection == "Information":
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
+    # Building out the "Information" page
+    if selection == "Information":
+        st.info("General Information")
+        # You can read a markdown file from supporting resources folder
+        st.markdown("Some information here")
 
-		st.subheader("Raw Twitter data and label")
-		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+        st.subheader("Raw Twitter data and label")
+        if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+            st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
-	# Building out the predication page
-	if selection == "Prediction":
-		st.info("Prediction with ML Models")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text",str("Type Here"))
+    # Building out the predication page
+    if selection == "Prediction":
+        st.info("Prediction with ML Models")
+        # Creating a text box for user input
+        tweet_text = st.text_area("Enter Text",str("Type Here"))
 
-	if st.button("Classify"):
-			   # Transforming user input with vectorizer
-			    vect_text = tweet_cv.transform([pre_process(tweet_text)]).toarray()
-			    # Load your .pkl file with the model of your choice + make predictions
-			    # Try loading in multiple models to give the user a choice
-			    predictor = joblib.load(open(os.path.join("resources/trained.pkl"),"rb"))
-			    prediction = predictor.predict(vect_text)
+    if st.button("Classify"):
+               # Transforming user input with vectorizer
+                vect_text = tweet_cv.transform([pre_process(tweet_text)]).toarray()
+                # Load your .pkl file with the model of your choice + make predictions
+                # Try loading in multiple models to give the user a choice
+                predictor = joblib.load(open(os.path.join("resources/trained.pkl"),"rb"))
+                prediction = predictor.predict(vect_text)
 
-			    # When model has successfully run, will print prediction
-			    # You can use a dictionary or similar structure to make this output
-			    # more human interpretable.
-			    st.success("Text Categorized as: {}".format(prediction))
+                # When model has successfully run, will print prediction
+                # You can use a dictionary or similar structure to make this output
+                # more human interpretable.
+                st.success("Text Categorized as: {}".format(prediction))
 
 
 # Required to let Streamlit instantiate our web app.  
