19fe06126501ec629112b1f8496a9fad697773f5
Rikus Kruger <rikusetiennekruger@gmail.com>
2021-06-24 13:54:03 +0200

    Added video and new page
    Model and data description page and tutorial video

--- base_app.py
+++ base_app.py
@@ -28,6 +28,7 @@ import joblib
 import pandas as pd
 # Streamlit dependencies
 import streamlit as st
+import cv2
 
 
 # Preprocessing dependencies
@@ -38,7 +39,9 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import string
 import re
-import nltk
+import io
+from PIL import Image
+
 # nltk.download('punkt')
 # nltk.download('stopwords')
 
@@ -118,36 +121,75 @@ def main():
 
     # Creating sidebar with selection box -
     # you can create multiple pages this way
-    options = ["Prediction", "Tutorial", "Model Information"]
+    options = ["Prediction", "Tutorial", "Model and Data Information"]
     selection = st.sidebar.selectbox("Choose Option", options)
 
     # Defining model descriptions
-    model_paths = {"Random Forest": rfc_model,
+    model_paths = {"Logistic Regression": lr_model,
                    "LinearSVC": lsvc_model, 
-                   "Logistic Regression": lr_model,
                    "Support Vector Classifier": svc_model,
-                   "MultinomialNB" : mnb_model}
+                   "MultinomialNB" : mnb_model,
+                   "Random Forest": rfc_model}
 
-    model_descriptions = {"Random Forest": "Random forest models are an example of an ensemble method that is built on decision trees (i.e. it relies on aggregating the results of an ensemble of decision trees). Decision tree machine learning models represent data by partitioning it into different sections based on questions asked of independent variables in the data. Training data is placed at the root node and is then partitioned into smaller subsets which form the 'branches' of the tree. In random forest models, the trees are randomized and the model returns the mean prediction of all the individual trees",
+    model_descriptions = {
+                          "Logistic Regression": "Logistic regression is a statistical model that makes use of a logistic function to model \
+                           a binary dependent variable, however, multiclass classification with logistic regression can be done through the \
+                           one-vs-rest scheme in which a separate model is trained for each class to predict whether an observation is that \
+                           class or not (thus making it a binary classification problem)",
 
-                          "LinearSVC":  "The objective of a Linear Support Vector Classifier is to return a 'best fit' hyperplane that categorises the data. It is similar to SVC with the kernel parameter set to ’linear’, but it is implemented in terms of liblinear rather than libsvm, so it has more flexibility in the choice of penalties and loss functions and can scale better to large numbers of samples.",
+                          "LinearSVC":  "The objective of a Linear Support Vector Classifier is to return a 'best fit' hyperplane that categorises\
+                           the data. It is similar to SVC with the kernel parameter set to ’linear’, but it is implemented in terms of liblinear rather\
+                           than libsvm, so it has more flexibility in the choice of penalties and loss functions and can scale better to large numbers of samples.",
 
-                          "Support Vector Classifier" : "A Support Vector Classifier is a discriminative classifier formally defined by a separating hyperplane. When labelled training data is passed to the model, also known as supervised learning, the algorithm outputs an optimal hyperplane which categorizes new data",
+                          "Support Vector Classifier" : "A Support Vector Classifier is a discriminative classifier formally defined by a separating hyperplane.\
+                           When labelled training data is passed to the model, also known as supervised learning, the algorithm outputs an optimal hyperplane\
+                           which categorizes new data",
 
-                          "Logistic Regression": "Logistic regression is a statistical model that makes use of a logistic function to model a binary dependent variable, however, multiclass classification with logistic regression can be done through the one-vs-rest scheme in which a separate model is trained for each class to predict whether an observation is that class or not (thus making it a binary classification problem)",
+                          "MultinomialNB": "The multinomial Naive Bayes classifier is suitable for classification with discrete features\
+                           (e.g., word counts for text classification)",
 
-                          "MultinomialNB": "The multinomial Naive Bayes classifier is suitable for classification with discrete features (e.g., word counts for text classification)"}
+                          "Random Forest": "Random forest models are an example of an ensemble method that is built on decision trees\
+                           (i.e. it relies on aggregating the results of an ensemble of decision trees). \
+                           Decision tree machine learning models represent data by partitioning it into different sections based on questions asked \
+                           of independent variables in the data. Training data is placed at the root node and is then partitioned into smaller subsets \
+                           which form the 'branches' of the tree. In random forest models, the trees are randomized and the model returns the mean prediction\
+                           of all the individual trees"
+                        }
 
-    # Building out the "Information" page
-    if selection == "Tutorial":
-        st.info("Tutorial")
-        # You can read a markdown file from supporting resources folder
-        st.markdown("Watch the video below to learn how to use the Tweet Classifier")
+    
+    model_accuracies = {"Logistic Regression": 0.7605,
+                        "LinearSVC": 0.7593, 
+                        "Support Vector Classifier": 0.7491,
+                        "MultinomialNB" : 0.7042,
+                        "Random Forest": 0.6905}
+    
+    
+    if selection == "Model and Data Information":
+        
+        chosen = st.radio('Model Selection', (list(model_descriptions.keys())))
+        st.subheader(f"{chosen} (Accuracy: {round(model_accuracies[chosen]*100)}%)")
+        st.markdown(model_descriptions[chosen])
 
-        st.subheader("Raw Twitter data and label")
+        image = Image.open('resources\imgs\Accuracy_scores.png')
+        st.image(image, caption='Model Accuracies (F1-score)')
+        st.markdown("Raw Twitter data and label")
         if st.checkbox('Show raw data'):  # data is hidden if box is unchecked
             # will write the df to the page
             st.write(raw[['sentiment', 'message']])
+        
+
+    # Building out the "Information" page
+    if selection == "Tutorial": 
+
+        st.title("Tutorial")
+        
+
+        st.markdown("Watch the video below to learn how to use the Tweet Classifier")
+
+        video_file = open('resources\Tutorial.mp4', 'rb')
+        video_bytes = video_file.read()
+        st.video(video_bytes)
+
 
     # Building out the predication page
     if selection == "Prediction":
@@ -155,28 +197,18 @@ def main():
             st.info("Prediction with a selection of ML Models")
             chosen = st.radio('Model Selection', (list(model_paths.keys())))
 			# Creating a text box for user input
+            st.subheader(f"{chosen} (Accuracy: {round(model_accuracies[chosen]*100)}%)")
             tweet_text = st.text_area("Type or paste your tweet here:", "Type Here")
             if st.button("Classify"):
                 # Transforming user input with vectorizer
                 
                 text = tweet_text.lower()
-                # st.markdown(f"lower: \n{text}")
                 
                 text = remove_urls(text)
-                # st.markdown(f"remove_urls: \n{text}")
 
                 text = remove_punctuation_numbers(text)
-                # st.markdown(f"remove_punctuation_numbers: \n{text}")
 
-                # text = rm_stopwords(text)
-                # st.markdown(f"rm_stopwords: \n{text}")
-                # text = listToString(text)
-                # st.markdown(f"listToString: \n{text}")
-                # text = removeNonAscii(text)
-
-                # st.markdown(f"removeNonAscii: \n{text}")
                 vect_text = tfidf_vect.transform([text])
-                # st.markdown(f"vect_text: \n{text}")
 
                 # Load your .pkl file with the model of your choice + make predictions
                 # Try loading in multiple models to give the user a choice
@@ -188,19 +220,18 @@ def main():
                 # You can use a dictionary or similar structure to make this output
                 # more human interpretable.
                 if prediction == 2:
-                    st.success("This tweet links to factual news about climate change.")
+                    st.warning("This tweet links to factual news about climate change.")
 
                 elif prediction == 1:
                     st.success("This tweet supports the belief of a man-made climate change.")
 
                 elif prediction == 0:
-                    st.success("This tweet neither supports nor refutes the belief of a man-made climate change.")
+                    st.info("This tweet neither supports nor refutes the belief of a man-made climate change.")
 
                 elif prediction == -1:
-                    st.success("This tweet does not believe in a man made climate change")
+                    st.error("This tweet does not believe in a man made climate change")
 
-            st.subheader(f"{chosen}")
-            st.markdown(f"Description from documentation: \n\n {model_descriptions[f'{chosen}']}")
+            
 
 
 
