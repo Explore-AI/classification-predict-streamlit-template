f5452a462b4b234eadd136cf41a8014a49769f4f
Jack Kamire <69396901+Jackam-spec@users.noreply.github.com>
2022-04-04 16:41:22 +0300

    data insights page

--- base_app.py
+++ base_app.py
@@ -27,8 +27,13 @@ import joblib,os
 
 # Data dependencies
 import pandas as pd
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS , ImageColorGenerator
 import matplotlib.pyplot as plt
+import string
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 # Vectorizer
 news_vectorizer = open("resources/tfidfvect.pkl","rb")
@@ -36,6 +41,7 @@ tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl f
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
+df_train = pd.read_csv("data/train.csv")
 
 # The main function where we will build the actual app
 def main():
@@ -82,14 +88,61 @@ def main():
 
 	# Building the EDA Page
 	if selection == "Data Insights":
-		# Generate Wordcloud imaages
-		wordcloud = WordCloud().generate(raw['message'])
+		st.title("Data Insights")
+		# Clean Data
+		wn = WordNetLemmatizer()
 
-		# Display image
-		plt.imshow(wordcloud, interpolation='bilinear')
-		plt.axis('off')
-		plt.show()
-		st.pyplot()
+		def text_preprocessing(review):
+			""" Takes into text data and preprocesses before returning text data."""
+			review = re.sub('[^a-zA-Z]', ' ', review)
+			review = review.lower()
+			review = review.split()
+			review = [wn.lemmatize(word) for word in review if not word in stopwords.words('english')]
+			review = ' '.join(review)
+			return review
+
+		# Clean data
+		df_train['message']= df_train['message'].apply(text_preprocessing)
+		df = df_train.copy()
+
+		# Generate Wordcloud imaages
+		# Create DataFrame for Each Sentiment
+		df_sent1 = df[df['sentiment']==1]
+		df_sent0 = df[df['sentiment']==0]
+		df_sentneg = df[df['sentiment']==-1]
+		df_sent2 = df[df['sentiment']==2]
+
+		tweet_All = " ".join(review for review in df_train.message)
+		tweet_sent0 = " ".join(review for review in df_sent0.message)
+		tweet_sent1 = " ".join(review for review in df_sent1.message)
+		tweet_sentneg = " ".join(review for review in df_sentneg.message)
+		tweet_sent2 = " ".join(review for review in df_sent2.message)
+
+		fig, ax = plt.subplots(5, 1, figsize  = (30,30))
+		# Create and generate a word cloud image:
+		wordcloud_ALL = WordCloud(max_font_size=50, max_words=100, background_color="black").generate(tweet_All)
+		wordcloud_sent0 = WordCloud(max_font_size=50, max_words=100, background_color="black").generate(tweet_sent0)
+		wordcloud_sent1 = WordCloud(max_font_size=50, max_words=100, background_color="black").generate(tweet_sent1)
+		wordcloud_sentneg = WordCloud(max_font_size=50, max_words=100, background_color="black").generate(tweet_sentneg)
+		wordcloud_sent2 = WordCloud(max_font_size=50, max_words=100, background_color="black").generate(tweet_sent2)
+
+		# Display the generated image:
+		ax[0].imshow(wordcloud_ALL, interpolation='bilinear')
+		ax[0].set_title('All Tweets', fontsize=30)
+		ax[0].axis('off')
+		ax[1].imshow(wordcloud_sent0, interpolation='bilinear')
+		ax[1].set_title('Neutral Tweets',fontsize=30)
+		ax[1].axis('off')
+		ax[2].imshow(wordcloud_sent1, interpolation='bilinear')
+		ax[2].set_title('Pro Climate Change',fontsize=30)
+		ax[2].axis('off')
+		ax[3].imshow(wordcloud_sentneg, interpolation='bilinear')
+		ax[3].set_title('Anti Climate Change',fontsize=30)
+		ax[3].axis('off')
+		ax[4].imshow(wordcloud_sent2, interpolation='bilinear')
+		ax[4].set_title('News Tweets',fontsize=30)
+		ax[4].axis('off')
+		st.pyplot(fig)
 
 
 # Required to let Streamlit instantiate our web app.  
