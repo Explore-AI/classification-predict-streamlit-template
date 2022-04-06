27f7201d7017e6dc98c5fa643bf8b65f0b1f5a0a
colinmburugu <colinmburugu@gmail.com>
2022-04-06 18:28:15 +0300

    changes on juju classifier

--- base_app.py
+++ base_app.py
@@ -51,13 +51,7 @@ tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl f
 vectorizer = open("resources/tfidf_vectorizer.pkl","rb")
 vectors = joblib.load(vectorizer)
 
-#Images
-logo = Image.open("resources/imgs/juju.jpg")
-kanu = Image.open("resources/imgs/kanu.jpeg")
-mburugu = Image.open("resources/imgs/mburugu.jpeg")
-mercy = Image.open("resources/imgs/mercy.jpeg")
-ogaga = Image.open("resources/imgs/ogaga.jpeg")
-faith = Image.open("resources/imgs/faith.jpeg")
+
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
@@ -79,7 +73,7 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information","EDA","About us"]
+	options = ["Prediction", "Information","EDA","About App"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
@@ -142,114 +136,38 @@ def main():
 		    st.pyplot()
 
 	#Building About us page
-	if selection == 'About us':
+	if selection == 'About App':
 		with st.container():
 		    st.write("---")
-		    left_column, right_column = st.columns(2)
-		    with left_column:
-		        st.header("About us")
-		        st.write("##")
-		        st.write(
+		    # left_column, right_column = st.columns(2)
+		    # with left_column:
+		    st.header("About App")
+		    st.write("##")
+		    st.write(
 		            """
-		            - The company was founded in 2015
+		            Here in Africa, Juju is a dreaded name for things that are perceived to be with Suprenatural or magical abilities.
 
-		            - The driving force of the company is to help start ups build data models to solve complex problems
 
-		            - We have helped hundreds of different companies make great business decisions using excellent data models
+		            Diving into the roots of Mama Africa and tapping from her abundance of resources , we present to the world JUJU Classifier
+
+		            Our JUJU Classifier has a supernatural and magical ability of making predictions from tweet sentiments. It uses the power of Machine Learning and Natural Language Processing to categorize tweets into any of 4 different labels (-1, 0, 1,2).
+
+		            JUJU Classifier is one of the many prediction tools developed by TEAM NATURE, an indigenous data science company whose mission is helping clients make data-driven decisions to boost their business growth.
+
 		            """
 		        )
-		    with right_column:
+		    # with right_column:
 
 		    	 
-	        	 st.image(logo)
+	        	 # st.image(logo)
 
 
-		with st.container():
-		    st.write("---")
-		    st.header("Company Owners")
-		    st.write("##")
-		    image_column, text_column = st.columns((1, 5))
-		    with image_column:
-		        st.image(mburugu)
-		    with text_column:
-		        st.subheader("Mburugu - Chief Executive Officer")
-		        st.write(
-		            """
-		            Incharge of all operations the company.
-		            He is also the biggest share holder
-		            """
-		        )
-		        
-		with st.container():
-		    image_column, text_column = st.columns((1, 5))
-		    with image_column:
-		        st.image(kanu)
-		    with text_column:
-		        st.subheader("Ogaga - Director")
-		        st.write(
-		            """
-		            -Monitoring progress towards achieving the objectives and policies.
-
-		            - Appointing senior management.
-		            """
-		        )
-
-
-		with st.container():
-		    image_column, text_column = st.columns((1, 5))
-		    with image_column:
-		        st.image(kanu)
-		    with text_column:
-		        st.subheader("Mercy  - Manager")
-		        st.write(
-		            """
-		            - Supervises company's progress.
-
-		            - Planning for the company.
-		            """
-		        ) 
-
-		with st.container():
-		    image_column, text_column = st.columns((1, 5))
-		    with image_column:
-		        st.image(kanu)
-		    with text_column:
-		        st.subheader("Kanu  - Engineer")
-		        st.write(
-		            """
-		            - Ensures the model is in check and that it generalizes
-
-		            -Performs juju if the model doesn't perform
-
-		            
-		            """
-		        )   
-
-
-		with st.container():
-		    image_column, text_column = st.columns((1, 5))
-		    with image_column:
-		        st.image(kanu)
-		    with text_column:
-		        st.subheader("Faith  - RelationShip Manager")
-		        st.write(
-		            """
-		            - Ensures people relate well in their respective work stations
-		            
-
-		            
-		            """
-		        )   
-
-
-
-		        
-
+		
 
 	# Building out the predication page
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
-		models = ['Logistic_regression','Naive Bayes','Bagging_Classifier']
+		models = ['Logistic_regression','Naive Bayes','Bagging_Classifier','Random_Forest']
 		clf = st.sidebar.selectbox("Select a Classifer", models)
 
 		# Creating a text box for user input
@@ -259,86 +177,9 @@ def main():
 
 		if st.button("Classify"):
 			# Transforming user input with vectorizer
-			# vect_text = tweet_cv.transform([tweet_text]).toarray()
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
 
-			## Remove urls
-
-			pattern_url = r'http[s]?://(?:[A-Za-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9A-Fa-f][0-9A-Fa-f]))+'
-			subs_url = r'url-link'
-			raw['clean'] = raw['message'].replace(to_replace = pattern_url, value = subs_url, regex = True)
-
-			# Make lower case
-
-			raw['clean'] = raw['clean'].str.lower()
-
-			#Punctuations removal:
-
-			def remove_punctuation_numbers(post):
-			    punc_numbers = string.punctuation + '0123456789'
-			    return ''.join([l for l in post if l not in punc_numbers])
-			raw['clean'] = raw['clean'].apply(remove_punctuation_numbers)
-
-			#Removed NonAscii
-
-			def _removeNonAscii(s): 
-			    return "".join(i for i in s if ord(i)<128)
-
-			raw['clean'] = raw['clean'].apply(_removeNonAscii)
-
-			# #Removes retweets and tweeted at
-			# def remove_users(col):
-
-			#     col = re.sub('(rt\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', col) # remove retweet
-			#     col = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', col) # remove tweeted at
-			#     return col
-
-			# raw['clean'] = raw['clean'].apply(remove_users) 
-
-
-
-			# Clean the word using stemming
-			stema = PorterStemmer()
-			raw['clean'] = [stema.stem(word) for word in raw['clean']]
-
-			# #Stopword removal:
-
-			raw['clean'] = [word for word in raw['clean'] if word not in ENGLISH_STOP_WORDS]
-
-
-			# raw_1 = raw[raw['sentiment']== 1]
-			# raw_2= raw[raw['sentiment']== 2]
-			# raw_0 = raw[raw['sentiment']== 0]
-			# raw_n = raw[raw['sentiment']== -1]
-
-			# base = 5000
-
-
-   # #          # UPSAMPLE SENTIMENTS
-			# neutral_sampled3 = resample(raw_0,
-			#                           replace=True, # sample with replacement (we need to duplicate observations)
-			#                           n_samples= base, # match number in minority class
-			#                           random_state=27) # reproducible results
-
-			# pro_sampled3 = resample(raw_1,
-			#                           replace=False, # sample with replacement (we dont need to duplicate observations)
-			#                           n_samples= base, # match number in minority class
-			#                           random_state=27) # reproducible results
-
-			# news_sampled3 = resample(raw_2,
-			#                           replace=True, # sample with replacement (we need to duplicate observations)
-			#                           n_samples= base, # match number in minority class
-			#                           random_state=27)
-
-			# anti_sampled3 = resample(raw_n,
-			#                           replace=True, # sample with replacement (we need to duplicate observations)
-			#                           n_samples= base, # match number in minority class
-			#                           random_state=27)
-
-			# train3 = pd.concat([neutral_sampled3, pro_sampled3,news_sampled3,anti_sampled3])
-
-			features = raw['clean']
-
-			X = vectors.transform(features)
+			X = vectors.transform([tweet_text])
 
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
@@ -354,10 +195,17 @@ def main():
 					st.success("Text Categorized as: {}".format(prediction))
 
 			elif clf == 'Bagging_Classifier':
+					predictor = joblib.load(open(os.path.join("resources/twitter_simple_bag_model_2.pkl"),"rb"))
+					prediction = predictor.predict(X)
+
+					st.success("Text Categorized as: {}".format(prediction))
+
+			elif clf == 'Random_Forest':
 					predictor = joblib.load(open(os.path.join("resources/twitter_simple_rf_model.pkl"),"rb"))
 					prediction = predictor.predict(X)
 
-					st.success("Text Categorized as: {}".format(X))
+					st.success("Text Categorized as: {}".format(prediction))
+
 
 
 
