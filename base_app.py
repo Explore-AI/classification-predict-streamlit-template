a8a446ebccc88b7ee425ad404752de2ae57731b1
stlankiro <69773962+stlankiro@users.noreply.github.com>
2022-06-29 12:35:02 +0200

    Added model and 2 empty pages

--- base_app.py
+++ base_app.py
@@ -30,7 +30,7 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
 # Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
+news_vectorizer = open("resources/countVectr1.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
@@ -144,7 +144,7 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information", "Data Cleaning and Analysis"]
+	options = ["Prediction", "Information", "Data Cleaning and Analysis", "About us", "Contact us"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
@@ -157,24 +157,45 @@ def main():
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
+    # Building out the "About us" page
+	if selection == "About us":
+		st.subheader("About us")
+		st.info("About us")
+		# You can read a markdown file from supporting resources folder
+		st.markdown("Some information here")
+
+	# Building out the "Contact us" page
+	if selection == "Contact us":
+		st.info("Contact us")
+		# You can read a markdown file from supporting resources folder
+		st.markdown("Some information here")
+
+    #models = ["Prediction", "Information", "Data Cleaning and Analysis", "About us", "Contact us"]
+	    #select_mod = st.selectbox("Choose Option", models)
+
 	# Building out the predication page
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
 
+        #models = ["Prediction", "Information", "Data Cleaning and Analysis", "About us", "Contact us"]
+	    #select_mod = st.selectbox("Choose Option", models)
+
 		if st.button("Classify"):
 			# Transforming user input with vectorizer
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			predictor = joblib.load(open(os.path.join("resources/logistic_model1.pkl"),"rb"))
 			prediction = predictor.predict(vect_text)
 
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
+
+            
 
 	# Building out the "Data Cleaning and Analysis" page
 	if selection == "Data Cleaning and Analysis":
