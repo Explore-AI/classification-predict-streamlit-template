9f3a038a03efb9a9ab59e0f0c87f8253c21791d3
Abdulr3 <112653767+Abdulr3@users.noreply.github.com>
2022-12-01 23:19:16 +0100

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -46,7 +46,7 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Prediction","Data Insights", "Information"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
