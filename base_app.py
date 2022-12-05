40b3622d6c254863d87da802c64fb0eec0e7461a
nmraqisa <112183163+nmraqisa@users.noreply.github.com>
2022-12-05 09:50:03 +0200

    git committed

--- base_app.py
+++ base_app.py
@@ -27,6 +27,12 @@ import joblib,os
 
 # Data dependencies
 import pandas as pd
+import numpy as np
+import pydeck as pdk
+import matplotlib.pyplot as plt
+
+
+
 
 # Vectorizer
 news_vectorizer = open("resources/tfidfvect.pkl","rb")
@@ -46,7 +52,7 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Prediction", "Information", "Visualization", "Dashboard"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
@@ -55,15 +61,86 @@ def main():
 		# You can read a markdown file from supporting resources folder
 		st.markdown("Some information here")
 
+	
+
 		st.subheader("Raw Twitter data and label")
-		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+	if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+
+	# Visualisation
+	if selection == "Visualization":
+		st.subheader("Sentiment distribution")
+		sentiment_dist = pd.DataFrame(raw['sentiment'].value_counts()).head()
+		st.bar_chart(sentiment_dist)
+
+		st.subheader("A wide column with a chart")
+		st.line_chart(raw['sentiment'])
+
+		st.subheader("A narrow column with the data")
+		st.write(raw['sentiment'])
+
+
+	# Dashboard experiment
+	if selection == "Dashboard":
+		st.subheader("Sentiment distribution")
+		sentiment_dist = pd.DataFrame(raw['sentiment'].value_counts()).head()
+		fig, ax = plt.subplots()
+		ax.hist(sentiment_dist, bins=20)
+		st.pyplot(fig)
+
+		values = st.slider(
+		'Select sentiment',
+    	-1, 2, (0, 1))
+		st.write('Values:', values)
+
+
+		chart_data = pd.DataFrame(
+   		np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   		columns=['sentiment', 'message'])
+
+		st.pydeck_chart(pdk.Deck(
+   	 	map_style=None,
+    	initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[message, sentiment]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
+
+
+
+	
+	
 
 	# Building out the predication page
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
+		st.markdown("You can enter text or upload file ")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
+
+		# upload a file
+		upload_file = st.file_uploader("Upload file")
 
 		if st.button("Classify"):
 			# Transforming user input with vectorizer
@@ -72,6 +149,11 @@ def main():
 			# Try loading in multiple models to give the user a choice
 			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
 			prediction = predictor.predict(vect_text)
+
+			# rfc model
+			#predictor = joblib.load(open(os.path.join("resources/rfc_model.pkl"),"rb"))
+			#prediction = predictor.predict(vect_text)
+
 
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
