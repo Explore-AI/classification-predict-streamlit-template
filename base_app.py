fc03eddf497500cc77e3a779282f48ea50dbc1ff
nmraqisa <112183163+nmraqisa@users.noreply.github.com>
2022-12-11 18:59:55 +0200

    updated app template

--- base_app.py
+++ base_app.py
@@ -31,9 +31,33 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import matplotlib.pyplot as plt
+import plotly.express as px
+
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+background-image: url('https://anhdepfree.com/wp-content/uploads/2019/05/50-anh-background-dep-nhat-4.jpg');
+background-size: cover;
+}
+[data-testid="stHeader"] {
+background-color: rgba(0,0,0,0);
+
+}
+
+[data-testid="stToolbar"] {
+right: 2rem;
+}
+
+[data-testid="stSidebar"] {
+background-image: url('https://images.pexels.com/photos/2088203/pexels-photo-2088203.jpeg?auto=compress&cs=tinysrgb&w=600');
+background-size: cover;
+}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
- 
+
 
 # Vectorizer
 news_vectorizer = open("resources/tfidfvect.pkl","rb")
@@ -51,124 +75,94 @@ def main():
 	st.title("Tweet Classifer")
 	st.subheader("Climate change tweet classification")
 
-	# Creating sidebar with selection box -
-	# you can create multiple pages this way
-	options = ["Information", "Prediction", "RFC_Model", "Visualization", "Dashboard"]
+	st.info("This application is all about tweet sentiment analysis of climate change. It is able to classify whether" 
+			 "or not a person believes in climate change, based on their novel tweet data.")
+	#st.()
+		# You can read a markdown file from supporting resources folder
+		#st.markdown("")
+	if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+		st.write(raw[['sentiment', 'message']]) # will write the df to the page
+
+
+    # Creating sidebar with selection box -
+    # you can create multiple pages this way
+	options = ["Home", "About us", "App tour", "Tweet classifier", "Tweet analysis"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
-	# Building out the "Information" page
-	if selection == "Information":
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
-
-	
-
-		st.subheader("Raw Twitter data and label")
-	if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['sentiment', 'message']]) # will write the df to the page
-
-	# Visualisation
-	if selection == "Visualization":
-		st.subheader("Sentiment distribution")
-		sentiment_dist = pd.DataFrame(raw['sentiment'].value_counts()).head()
-		st.bar_chart(sentiment_dist)
-
-		st.subheader("A wide column with a line chart")
-		st.line_chart(raw['sentiment'])
-
-		st.subheader("A narrow column with the data")
-		st.write(raw['sentiment'])
-
-
-	# Dashboard experiment
-	if selection == "Dashboard":
-		st.subheader("Sentiment distribution")
-		sentiment_dist = pd.DataFrame(raw['sentiment'].value_counts()).head()
-		fig, ax = plt.subplots()
-		ax.hist(sentiment_dist, bins=20)
-		st.pyplot(fig)
-
-		values = st.slider(
-		'Select sentiment',
-    	-1, 2, (0, 2))
-		st.write('Values:', values)
-
-
-		chart_data = pd.DataFrame(
-   		np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-   		columns=['sentiment', 'message'])
-
-		st.pydeck_chart(pdk.Deck(
-   	 	map_style=None,
-    	initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=chart_data,
-           get_position='[message, sentiment]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
-
-
-
-	
-	
-
-	# Building out the predication page
-	if selection == "Prediction":
+	# Building out the "Tweet Sentitment classification " page
+	if selection == "Tweet classifier":
 		st.info("Prediction with ML Models")
-		st.markdown("You can enter text or upload file ")
-		# Creating a text box for user input
+		st.markdown("You can enter text or upload file")
+        # Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
 
 		# upload a file
 		upload_file = st.file_uploader("Upload file")
 
 		if st.button("Classify"):
-			# Transforming user input with vectorizer
+            # Transforming user input with vectorizer
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			#predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
-			#prediction = predictor.predict(vect_text)
-
-			# Random classifier model
-	if selection == "RFC_Model":
-			st.markdown("You can enter text or upload file ")
-			# Creating a text box for user input
-			tweet_text = st.text_area("Enter Text","Type Here")
-
-	if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# rfc model
-			predictor = joblib.load(open(os.path.join("resources/rfc_model.pkl"),"rb"))
+            # Load your .pkl file with the model of your choice + make predictions
+            # Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
 			prediction = predictor.predict(vect_text)
 
 
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
+	# Building out the "Tweet Sentitment analysis " page		
+	if selection == "Tweet analysis":
+		st.info("This app analyses sentiments on climate change based on tweet data")
+		#top level filters
+		#message_filter = st.selectbox("Select the message", pd.unique(raw['sentiment']))
+		# dataframe filter
+		#df = raw[raw['sentiment']== message_filter] 
+		st.markdown("### Tweet distribution")
+		sentiment = raw['sentiment'].value_counts()
+		sentiment = pd.DataFrame({'Sentiment':sentiment.index, 'Tweets':sentiment.values})
+	
+		# create two columns for charts
+		fig_col1, fig_col2 = st.columns(2)
+		
+		with fig_col1:
+			fig = fig = px.bar(sentiment, x='Sentiment', y='Tweets', color = 'Tweets', height= 500)
+			st.plotly_chart(fig)
+	
+       	#
+		with fig_col2:
+			fig = px.pie(sentiment, values= 'Tweets', names= 'Sentiment')
+			st.plotly_chart(fig)
+			
+	
+
+		
+
+	
+		
+
+
+
+
+
+
+	
+		
+
+			
+	
+		#with fig_col1:
+		#	st.markdown("### First Chart")
+		#	fig = px.density_heatmap(
+       	#	data_frame=df, y="message", x="sentiment"
+    	#	)
+		#	st.write(fig)
+   
+		
+		#	fig2 = px.histogram(data_frame=df, x="sentiment")
+		#	st.write(fig2)
+
+		
+
+
+	
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
