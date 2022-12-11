473537b9016dcb734e95bb94d7b8ea5dac56b48a
Caron_Sathekge <caronsathekge28@gmail.com>
2022-12-11 20:21:34 +0200

    Added information in Information, Home, & About Us

--- base_app.py
+++ base_app.py
@@ -38,7 +38,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, precision_score, recall_score
-from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menu   # To install: pip install streamlit-option-menu
 
 #from nltk.stem import WordNetLemmatizer
 #from nltk.corpus import stopwords
@@ -90,7 +90,7 @@ def main():
 	"""Tweet Classifier App with Streamlit """
 
 	# This is our company logo
-	st.image("resources/imgs/LeafLogo.png", caption='Our company logo')
+	#st.image("resources/imgs/LeafLogo.png", caption='Our company logo')
 	
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
@@ -102,19 +102,53 @@ def main():
 	#selection = st.sidebar.selectbox("", options)
 	#st.sidebar.info("General Information")
 	with st.sidebar:
-		selection = option_menu("Main Menu", ["Home", "About Us","Prediction", "Information", "Contact Us"], 
-        icons=['house', 'people-group','chart-line','info','address-book'], menu_icon="cast", default_index=1)
-    	
+		selection = option_menu("Main Menu", ["Home", "About Us", "Prediction", "Information", "Contact Us"], 
+        icons=['house', 'people-group','chart-line','info','address-book'], menu_icon="cast", default_index=0)
+																				# default_index = 0 (Home page)
+
+	# Building out the "About Us" page
+	if selection == "Home":
+		# This is our company logo
+		st.image("resources/imgs/LeafLogo.png", caption='Our company logo')
+
+		st.info("Climate Change")
+
+		st.markdown("Climate change refers to long-term shifts in temperatures and weather patterns. \
+					These shifts may be natural, or may be caused by human activities.")
+		st.markdown("==================================================================================")
+		st.markdown("Many companies are built around lessening one’s environmental impact or carbon footprint.\
+					They offer products and services that are environmentally friendly and sustainable, in \
+					line with their values and ideals. They would like to determine how people perceive\
+					climate change and whether or not they believe it is a real threat.")
+		st.markdown("The team was tasked in creating a Machine Learning model that is able to classify \
+					whether or not a person believes in climate change, based on their novel tweet data.")
+		st.markdown("The collection of this data was funded by a Canada Foundation for Innovation JELF Grant\
+					 to Chris Bauch, University of Waterloo. The dataset aggregates tweets pertaining to climate\
+					change collected between Apr 27, 2015 and Feb 21, 2018. In total, 43,943 tweets were collected. \
+					")
+		st.markdown("==================================================================================")
+
+
+	
 	# Building out the "About Us" page
 	if selection == "About Us":
+		# This is our company logo
+		st.image("resources/imgs/LeafLogo.png", caption='Our company logo')
 
 		# You can read a markdown file from supporting resources folder
 		st.title("Who Are We?")
 		st.subheader("Enviro Co.")
-		st.markdown('We are aaaaa a a aaaaaaaaaaaaa a a            a aa a aamah sgsgsg dfgdg gd dgf dg dg dg dg d gd g dg dg dg d gdg \
-					fgdgdgdg g dg dg dg dg dg dg dfffffffffffffffffffffdfdfdfdfffffffff sf fs fssssssss sfffffffffff  sf sfffffff fsfs \
-					adfsfsf sfffffffffff ssf  s               sfffffffffff              sfsfsf sf sf sf s fs f fs s fs f sf sf sf s    \
+		st.markdown('We are a company that deals with recycable products and materials. \
+					We are naturally concerned about `climate change` and how we can help the public lead and live a greener lifestyle. \
+					Most of what we do revolves around the full Data Science Life Cycle:   \
 					')
+		st.markdown(f"""
+				- Data Collection
+				- Data Cleaning
+				- Exploratory Data Analysis
+				- Model Building
+				- Model Deployment
+				""")
 		#st.subheader("Meet The Team")
 		st.markdown('<div style="text-align: center; color:Black; font-weight: bold; font-size: 30px;">Meet The Team</br></br></div>', unsafe_allow_html=True)
 
@@ -150,25 +184,11 @@ def main():
 			st.markdown('Nakedi')
 			st.image("https://static.streamlit.io/examples/owl.jpg")
 
-
-	# Building out the "Information" page
-	if selection == "Information":
-		st.title("Tweet Classifer")
-		st.subheader("Climate change tweet classification")
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
-
-		st.subheader("Raw Twitter data and label")
-		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['sentiment', 'message']]) # will write the df to the page
-
 	# Building out the predication page
 	if selection == "Prediction":
-		st.title("Tweet Classifer")
+		#st.title("Tweet Classifer")
 		st.subheader("Climate change tweet classification")
-		st.info("Prediction with ML Models")
-		#st.markdown('<div style="text-align: center;">Prediction with ML Models</div>', unsafe_allow_html=True)
+		st.info("Predicting with our best performing ML Model!")
 
 		# Creating a text box for user input
 		tweet_text = st.text_area(label="Enter Text", height= 250, help="Enter a text, then click on 'Classify' below", placeholder="Enter any text here")
@@ -185,16 +205,57 @@ def main():
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
-			st.success("Your text was categorized as: {}".format(prediction))
+			st.success("Your text was categorized as: {}".format(prediction), icon="✅")
+			st.balloons()
+			#st.snow()
 
-			if st.checkbox('See Category Meanings'):
-				st.markdown(f"""
-						**THE MEANING OF THESE CATEGORIES?**
-						- Category **-1** = Anti-climate change
-						- Category **0** = Neutral
-						- Category **1** = Pro climate change
-						- Category **2** = Factual News
-						""")
+		if st.checkbox('See Category Meanings'):
+			st.markdown(f"""
+				**THE MEANING OF THESE CATEGORIES?**
+				- Category **-1** = Anti-climate change
+				- Category **0** = Neutral
+				- Category **1** = Pro climate change
+				- Category **2** = Factual News
+				""")
+
+	# Building out the "Information" page
+	if selection == "Information":
+		#st.title("Tweet Classifer")
+		st.markdown('<div style="text-align: center; color:Black; font-size: 30px;">Some Information About The Data</div>', unsafe_allow_html=True)
+		st.markdown("")
+		st.markdown("")
+		st.markdown("")
+
+		#st.subheader("Climate change tweet classification")
+		st.info("A Bar Graph showing the number of tweets per sentiment")
+		# You can read a markdown file from supporting resources folder
+		#st.markdown("A Bar Graph showing the number of tweets per sentiment")
+		st.bar_chart(data=raw["sentiment"].value_counts(), x=None, y=None, width=220, height=320, use_container_width=True)
+
+		st.info("As it is observed from the bar graph above, it is well noted that many people \
+					support the belief of man-made climate change.")
+		st.markdown("")
+		st.markdown("")
+		st.markdown("")
+
+		st.subheader("Raw Twitter data")
+		if st.checkbox('View the raw data represented by the bar chart above'): # data is hidden if box is unchecked
+			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+		st.markdown("")
+		st.markdown("")
+		st.markdown("")
+
+		st.info("The Business Value of Our Product")
+		st.markdown("The model we have deployed successfully classifies a users tweet as either Anti, Neutral, Pro or Factual. \
+					This will help you, as a business, understand what your clients and potential clients are saying about \
+					climate change, and what they believe. As a business, it is vital to understand this information, so as \
+					to *customise* the type of advertisements you could show to each group of customers. This will in turn \
+					ensure that you bring the *right product* to the *right customer*, \
+					which is vital for the **success** of a business.\
+					")
+		st.markdown("")
+
+
     #A form for the Contact Us page
 	if selection == "Contact Us":
 		st.subheader("Contact")
