b6fce22a7f93256e63d08fe5cd7e06513d6919de
AndisiweJ <107894108+AndisiweJ@users.noreply.github.com>
2023-06-28 16:31:41 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -22,62 +22,110 @@
 
 """
 # Streamlit dependencies
+import json
+
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie 
 import joblib,os
+import os
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+from pycaret.classification import setup, compare_models, pull, save_model
+
 
 # Data dependencies
 import pandas as pd
+st.title("Tweet Classifer by AC2301-BM4")
+st.subheader("Climate change tweet classification")
+st.info("This application allows our clients to analyze the sentiments of potential customers as relates to climate change issues.")
+if os.path.exists("sourcedata.csv"):
+    df = pd.read_csv("sourcedata.csv", index_col = None)
+with st.sidebar:
+        choose =st.radio("Navigation Bar",["Upload","Preliminary Exploratory Data Analysis","ML Models","Download","Manual ML Model"])
+        
+if choose == "Upload":
+    st.subheader(" Upload your Dataset for Modelling")
+    file = st.file_uploader("Upload Your Dataset Here")
+    if file:
+        df= pd.read_csv(file,index_col=None)
+        df.to_csv("sourcedata.csv", index=None)
+        st.dataframe(df)
+if choose== "Exploratory Data Analysis":
+    st.subheader("Automated Exploratory Data Analysis")
+    profile_report= df.profile_report()
+    st_profile_report(profile_report)
+if choose== "ML Models":
+    st.subheader("Various Machine Learning Models")
+    target=st.selectbox("Select Your target", df.columns)
+    setup(df,target=target)
+    setup_df=pull()
+    st.info("This is the ML experiment setting")
+    st.dataframe(setup_df)
+    best_model=compare_models()
+    compare_df=pull()
+    st.info("This is the ML Nodel")
+    st.dataframe(compare_df)
+    best_model
+ 
+
+        
 
 # Vectorizer
 news_vectorizer = open("resources/tfidfvect.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
-raw = pd.read_csv("resources/train.csv")
+#raw = pd.read_csv("resources/train.csv")
+
+def load_lottieurl(url:str):
+    r =requests.get(url)
+    if r.status_code !=200:
+        return None
+    return r.json()
+lottie_animation =load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_cuKhxGQKFB.json")
+st_lottie("https://assets7.lottiefiles.com/packages/lf20_cuKhxGQKFB.json")
 
 # The main function where we will build the actual app
-def main():
-	"""Tweet Classifier App with Streamlit """
-
-	# Creates a main title and subheader on your page -
-	# these are static across all pages
-	st.title("Tweet Classifer")
-	st.subheader("Climate change tweet classification")
+#def main():
+#	#"""Tweet Classifier App with Streamlit """
+  	# Creates a main title and subheader on your page 
+    # these are static across all pages
+	#st.title("Tweet Classifer")
+	#st.subheader("Climate change tweet classification")
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
-	selection = st.sidebar.selectbox("Choose Option", options)
-
+	#options = ["Prediction", "Information"]
+	#selection = st.sidebar.selectbox("Choose Option", options)
 	# Building out the "Information" page
-	if selection == "Information":
-		st.info("General Information")
+	#if selection == "Information":
+		#st.info("General Information")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
-
-		st.subheader("Raw Twitter data and label")
-		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+		#st.markdown("Some information here")
+		#st.subheader("Raw Twitter data and label")
+		#if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+			#st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
 	# Building out the predication page
-	if selection == "Prediction":
-		st.info("Prediction with ML Models")
+	#if selection == "Prediction":
+		#st.info("Prediction with ML Models")
 		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text","Type Here")
+		#tweet_text = st.text_area("Enter Text","Type Here")
 
-		if st.button("Classify"):
+		#if st.button("Classify"):
 			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			#vect_text = tweet_cv.transform([tweet_text]).toarray()
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
+			#predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			#prediction = predictor.predict(vect_text)
 
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
+			#st.success("Text Categorized as: {}".format(prediction))
 
 # Required to let Streamlit instantiate our web app.  
-if __name__ == '__main__':
-	main()
+#if __name__ == '__main__':
+#	main()
